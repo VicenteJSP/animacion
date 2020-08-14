5dd46c06685fcf1199c7fc4ce2eb5406f7c1f681
Vicente Santiago <vicentejsp@outlook.com>
2020-08-14 17:41:47 -0500

    Primera animacion con mojs

--- src/app/components/mojs/mojs.component.ts
+++ src/app/components/mojs/mojs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import mojs from '@mojs/core';
 
 @Component({
   selector: 'app-mojs',
@@ -10,6 +11,49 @@ export class MojsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    new mojs.Shape({
+      parent: '#bouncy_circle',
+      shape: 'circle',
+      fill: { '#334752': '#46B39D' },
+      radius: { 80: 280 },
+      duration: 2000,
+      isYoyo: true,
+      isShowStart: true,
+      easing: 'elastic.inout',
+      repeat: 1,
+    })
+      .then({
+        fill: { '#46B39D' : '#F0CA4D' },
+        radius: { 80: 280 },
+        duration: 2000,
+        isYoyo: true,
+        isShowStart: true,
+        easing: 'elastic.inout',
+        repeat: 1,
+      })
+      .then({
+        fill: { '#F0CA4D': '#E37332' },
+        radius: { 80: 280 },
+        duration: 2000,
+        isYoyo: true,
+        isShowStart: true,
+        easing: 'elastic.inout',
+        repeat: 1,
+      })
+      .then({
+        fill: { '#E37332': '#DE4F3C' },
+        radius: { 80: 280 },
+        duration: 2000,
+        isYoyo: true,
+        isShowStart: true,
+        easing: 'elastic.inout',
+        repeat: 1,
+      })
+      .play()
+      .replay();
+
+
   }
 
 }

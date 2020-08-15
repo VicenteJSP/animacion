1900ae6dcdb740e24caa054d1ebc7c9cb32e66d0
Vicente Santiago <vicentejsp@outlook.com>
2020-08-15 16:53:41 -0500

    Configurando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TypedjsComponent } from './components/typedjs/typedjs.component';
+import { MojsComponent } from './components/mojs/mojs.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mojs', component: MojsComponent },
+  { path: 'typedjs', component: TypedjsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'mojs' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

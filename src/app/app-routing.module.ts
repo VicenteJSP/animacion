1bd03c347ce1836941a278473b127ffb2e68ec06
Vicente Santiago <vicentejsp@outlook.com>
2020-08-16 00:12:11 -0500

    Implentando primera animacion con ScrollReveal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { TypedjsComponent } from './components/typedjs/typedjs.component';
-import { MojsComponent } from './components/mojs/mojs.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScrollRevealComponent } from './components/scroll-reveal/scroll-reveal.component';
+import { TypedjsComponent } from './components/typedjs/typedjs.component';
+import { MojsComponent } from './components/mojs/mojs.component';
 
 const routes: Routes = [
   { path: 'mojs', component: MojsComponent },
   { path: 'typedjs', component: TypedjsComponent },
+  { path: 'scrollreveal', component: ScrollRevealComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'mojs' }
 ];
 
--- src/app/components/scroll-reveal/scroll-reveal.component.ts
+++ src/app/components/scroll-reveal/scroll-reveal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-scroll-reveal',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScrollRevealComponent implements OnInit {
 
+  opt: optScrollReveail;
   constructor() { }
 
   ngOnInit(): void {
+    this.opt = {
+      delay: 500,
+      duration: 1000,
+      reset: true,
+      origin: 'right',
+      rotate: { x: 100, y: 100, z:100 }
+    }
+    let scroll = ScrollReveal(this.opt);
+    scroll.reveal('.steps-10', { easing: 'steps(10)' });
+    scroll.reveal('.steps-20', { easing: 'steps(20)' });
+    scroll.reveal('.steps-40', { easing: 'steps(40)' });
+    scroll.reveal('.steps-60', { easing: 'steps(60)' });
   }
 
+}
+
+interface optScrollReveail {
+  delay?: number;
+  distance?: string;
+  duration?: number;
+  easing?: string;
+  interval?: number;
+  opacity?: number;
+  origin?: string;
+  rotate?: {
+    x?: number;
+    y?: number;
+    z?: number;
+  };
+  scale?: number;
+  cleanup?: boolean;
+  container?: any;
+  desktop?: boolean;
+  mobile?: boolean;
+  reset?: boolean;
+  useDelay?: string;
+  viewFactor?: number;
+  viewOffset?: {
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+  };
+  afterReset?: Function;
+  afterReveal?: Function;
+  beforeReset?: Function;
+  beforeReveal?: Function;
 }
